72b49594c28892176aa0d2d6b2c06935f6016ec3
Harsh veer singh <harshveer7828@gmail.com>
2024-02-13 23:12:59 +0530

    first commit

---
+++ socketapi.js
@@ -0,0 +1,15 @@
+const io = require( "socket.io" )();
+const socketapi = {
+    io: io
+};
+
+// Add your socket.io logic here!
+io.on( "connection", function( socket ) {
+    console.log( "A user connected" );
+    socket.on("groupchat",message=>{
+       socket.broadcast.emit("all",message);
+    });
+});
+// end of socket.io logic
+
+module.exports = socketapi;
